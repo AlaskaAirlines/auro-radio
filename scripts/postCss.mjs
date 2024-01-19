7a89702df1c4cec1438d520748ba56651b935b44
Dale Sande <dale.sande@gmail.com>
2024-01-18 22:05:29 -0800

    feat: add suport for SSR projects #117
    This commit updates scripts and configs to support SSR style projects.
    
    Changes to be committed:
    renamed:    commitlint.config.js -> commitlint.config.cjs
    modified:   demo/apiExamples.md
    modified:   package-lock.json
    modified:   package.json
    modified:   packageScripts/postinstall.mjs
    renamed:    scripts/generateDocs.js -> scripts/generateDocs.mjs
    renamed:    scripts/postCss.js -> scripts/postCss.mjs
    modified:   scripts/pre-commit.mjs

--- scripts/postCss.mjs
+++ scripts/postCss.mjs
@@ -1,9 +1,9 @@
-const autoprefixer = require('autoprefixer');
-const postcss = require('postcss');
-const comments = require('postcss-discard-comments');
-const fs = require('fs');
+import autoprefixer from 'autoprefixer';
+import postcss from 'postcss';
+import comments from 'postcss-discard-comments';
+import fs from 'fs';
 
-fs.readFile('src/auro-radio.css', (err, css) => {
+fs.readFile('src/style.css', (err, css) => {
   postcss([autoprefixer, comments])
     .use(comments({
       remove: function(comment) { return comment[0] == "@"; }

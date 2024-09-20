4d830b2c7960b7ef483d2942fe38596660d03a04
Jordan Jones <jmjones2463@gmail.com>
2024-09-20 11:34:40 -0700

    fix: update path to component js files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { AuroRadio } from '../src/auro-radio.js';
-import { AuroRadioGroup } from '../src/auro-radio-group.js';
+import { AuroRadio } from './src/auro-radio.js';
+import { AuroRadioGroup } from './src/auro-radio-group.js';
 
 import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 

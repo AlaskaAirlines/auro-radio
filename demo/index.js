5c28d898032933c3dd4c33ba212d940464ef3592
Eunsun Mota <eunsun.mota@alaskaair.com>
2024-10-23 17:06:59 -0700

    fix: make `demo/index.min.js` to load as a module

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,8 @@
 import { AuroRadio } from '../src/auro-radio.js';
 import { AuroRadioGroup } from '../src/auro-radio-group.js';
 
+AuroRadio.register();
+AuroRadioGroup.register();
+
 AuroRadio.register('custom-radio');
 AuroRadioGroup.register('custom-radio-group');
